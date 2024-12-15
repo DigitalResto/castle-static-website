5bbe1d8656188772f06a55eb88ac724b6b3ac0b2
Muhammed Anas NA <anasna6005@gmail.com>
2024-12-15 19:56:50 +0530

    Changed the rooturl dynamic

--- src/util/Axios/Endpoints/index.js
+++ src/util/Axios/Endpoints/index.js
@@ -1,4 +1,4 @@
-const rootURL = 'http://localhost:3000/api';
+const rootURL = process.env.ENVIRONMENT == 'dev' ? 'http://localhost:3000/api' : 'https://resto-nahdi.vercel.app/api'
 
 export const ADD_TO_WAITLIST_API = `${rootURL}/add-to-waitlist`;
 export const CHECK_OTP_API = `${rootURL}/check-otp`;
