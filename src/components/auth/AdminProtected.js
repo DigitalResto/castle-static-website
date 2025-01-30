25c6bfd34f958707f474064e921224dd88d09247
AdithyaSiva5 <adithyasiva111@gmail.com>
2025-01-30 23:15:41 +0530

    feedback fixed

--- src/components/auth/AdminProtected.js
+++ src/components/auth/AdminProtected.js
@@ -1,17 +1,24 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { checkAdminAuth } from '../../util/auth';
 
 export default function AdminProtected({ children }) {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!checkAdminAuth()) {
       router.push('/admin/login');
+    } else {
+      setIsLoading(false);
     }
   }, [router]);
+
+  if (isLoading) {
+    return <div>Loading...</div>; // You can replace this with a loading spinner or any other loading indicator
+  }
 
   return <>{children}</>;
 }
